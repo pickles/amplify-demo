53ab959cd78780d4a33a57990c5f27c82d869b3d
Manabu <manabu.shibata@gmail.com>
2024-03-22 15:51:41 +0000

    initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Amplify
+      </div>
+    </>
+  )
+}
+
+export default App
